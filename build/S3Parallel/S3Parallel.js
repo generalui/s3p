64919c89416aacd1545bec03df070eb01d79c9fb
shanebdavis <shanebdavis@gmail.com>
2021-07-14 07:53:56 -0700

    minor/feature: NEW: cli-equivelent API; find out how to use a CLI command from the API by executing your CLI command with the --api-example option

--- build/S3Parallel/S3Parallel.js
+++ build/S3Parallel/S3Parallel.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return [require('./Lib'), require('./S3P'), require('./S3Comprehensions')];});
+Caf.defMod(module, () => {return require('./S3PCliCommands');});
 //# sourceMappingURL=S3Parallel.js.map
