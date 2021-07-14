83d7c661dc42be876d287d482d11c3faec85b244
shanebdavis <shanebdavis@gmail.com>
2021-07-14 09:45:09 -0700

    patch/test: fixed tests

--- build/test/StandardImport.js
+++ build/test/StandardImport.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return require('art-standard-lib').mergeWithSelf(require('art-testbench'), require('../S3Parallel'));});
+Caf.defMod(module, () => {return require('art-standard-lib').mergeWithSelf(require('art-testbench'), require('../S3Parallel'), require('../S3Parallel').Lib, require('../S3Parallel').S3P, require('../S3Parallel').S3Comprehensions);});
 //# sourceMappingURL=StandardImport.js.map
